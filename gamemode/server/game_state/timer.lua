6c638128305999f24a1d2608251485fcd2f84649
TheHellBox <thehellbox11@gmail.com>
2019-10-06 13:01:43 +0500

    Fix timer

--- gamemode/server/game_state/timer.lua
+++ gamemode/server/game_state/timer.lua
@@ -12,7 +12,7 @@ hook.Add("Think", "Update timer", function()
 		game_state.time_left = math.Clamp(game_state.time_left - 1, 0, math.huge)
 		timer_next_update_time = CurTime() + 1
 		if game_state.time_left <= 0 then
-			game_state.time_left = 600
+			game_state.time_left = global_config.build_time
 			if game_state.state == GAME_STATE_BUILDING then
 				if table.Count(aw_teams_list) > 1 or aw_developer then
 					game_state.time_left = global_config.fight_time
